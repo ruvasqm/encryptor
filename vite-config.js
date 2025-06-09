df24ed2e096d16d55f8d58d353df830064681c92
Ruben Vasquez <ruvasqm@gmail.com>
2025-06-08 21:16:32 -0400

    add vite pwa plugin

--- vite-config.js
+++ vite-config.js
@@ -1,5 +1,6 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
 	// If deploying to GitHub Pages like: https://username.github.io/my-crypto-pwa-js/
@@ -7,4 +8,36 @@ export default defineConfig({
 	build: {
 		target: "esnext", // Good for modern browser features like SubtleCrypto
 	},
+	plugins: [
+		VitePWA({
+			injectRegister: "null",
+			includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+			manifest: {
+				version: "0.0.3",
+				manifest_version: 3,
+				name: "Encryptor | Web-based encryption tools",
+				short_name: "Encryptor",
+				description:
+					"Client-side encryption and hashing tool using OpenPGP.js and SubtleCrypto.",
+				start_url: ".",
+				display: "standalone",
+				background_color: "#ffffff",
+				theme_color: "#007bff",
+				icons: [
+					{
+						src: "vite.svg",
+						sizes: "192x192",
+						type: "image/svg+xml",
+						purpose: "any maskable",
+					},
+					{
+						src: "vite.svg",
+						sizes: "512x512",
+						type: "image/svg+xml",
+						purpose: "any maskable",
+					},
+				],
+			},
+		}),
+	],
 });
